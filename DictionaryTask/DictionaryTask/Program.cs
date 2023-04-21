60dd9338b8ebb89f7ae549a0a306281f0d56be8e
Andrej Marjanovic <andrej.marjanovic@outlook.com>
2023-04-21 13:16:21 +0200

    Initial task

---
+++ DictionaryTask/DictionaryTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using DictionaryTask.Service;
+
+class Program
+{
+    static void Main()
+    {
+        Actions();
+    }
+
+    static void Actions()
+    {
+        DictionaryService service = new DictionaryService();
+        service.DisplayDictionary();
+        service.FindOne();
+        service.ChangeToEnglish();
+        service.FindTuesday();
+    }
+}
---
+++ DictionaryTask/DictionaryTask/Service/DictionaryService.cs
@@ -0,0 +1,59 @@
+﻿using DictionaryTask.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryTask.Service
+{
+    public class DictionaryService
+    {
+        public void DisplayDictionary()
+        {
+            Console.WriteLine("Days of week are: ");
+            Console.WriteLine("-----------------");
+            foreach (var weekday in WeekDayStorage.WeekDays)
+            {
+                Console.WriteLine("day: {0}, Name: {1}", weekday.Key, weekday.Value);
+            }
+            Console.WriteLine();
+        }
+
+        public void FindOne()
+        {
+            var Day = WeekDayStorage.WeekDays.FirstOrDefault(x => x.Key == 1).Value;
+            if(Day != null)
+            {
+                Console.WriteLine("Day with the key 1 is: {0}", Day);
+            } 
+            Console.WriteLine();
+        }
+
+        public void ChangeToEnglish()
+        {
+            for (int i = WeekDayStorage.WeekDays.First().Key; i <= WeekDayStorage.WeekDays.Count; i++)
+            {
+                WeekDayStorage.WeekDays[i] = Enum.GetName(typeof(DayOfWeek), i % 7);
+            }
+
+            DisplayDictionary();
+        }
+
+        public void FindTuesday()
+        {
+            int DayKey = WeekDayStorage.WeekDays.FirstOrDefault(x => x.Value == "Tuesday").Key;
+            if (DayKey != 0)
+            {
+                WeekDayStorage.WeekDays.Remove(DayKey);
+            }
+
+            DisplayDictionary();
+        }
+
+        public void AddTuesday()
+        {
+            WeekDayStorage.WeekDays.Add(2, "Tuesday");
+        }
+    }
+}
